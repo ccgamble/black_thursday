a5f1649ad21a17e0d020157bc31337a769ca98a7
Lucy Fox <lucyreneefox@gmail.com>
2016-04-27 17:32:33 -0600

    invoice repository testing 100%

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,7 +1,11 @@
 require_relative 'test_helper'
 require_relative '../lib/invoice_repository'
+require_relative '../lib/sales_engine'
+
 
 class InvoiceRepositoryTest < Minitest::Test
+  attr_reader :inv
+
   def setup
     invoice1 = Invoice.new({
     :id          => 6,
@@ -23,10 +27,28 @@ class InvoiceRepositoryTest < Minitest::Test
 
     @invoice = InvoiceRepository.new
     @invoice.invoice_repository = ([invoice1, invoice2])
+
+    @se = SalesEngine.from_csv({
+      :items => "./data/items.csv",
+      :merchants => "./data/merchants.csv",
+      :invoices => "./data/invoices.csv",
+      :invoice_items => "./data/invoice_items.csv",
+      :transactions => "./data/transactions.csv",
+      :customers => "./data/customers.csv"})
+    @inv = @se.invoices
   end
 
   def test_it_creates_a_new_instance_of_invoice_repo
     assert_equal InvoiceRepository, @invoice.class
+  end
+
+  def test_it_inspects
+    assert @invoice.inspect
+  end
+
+  def test_it_starts_out_empty
+    ir = InvoiceRepository.new
+    assert ir.invoice_repository.empty?
   end
 
   def test_it_returns_an_array_of_all_invoice_instances
@@ -58,7 +80,43 @@ class InvoiceRepositoryTest < Minitest::Test
 
   def test_it_will_return_an_empty_array_if_merchant_id_doesnt_exist
     assert_equal [], @invoice.find_all_by_merchant_id(124)
+  end
 
+  def test_it_will_find_all_by_status
+    new_invoice = @invoice.find_all_by_status(:pending)
+    assert_equal 6, new_invoice[0].id
+  end
+
+  def test_it_will_find_merchant_by_invoice
+    merchant = inv.find_merchant_by_invoice_merch_id(12334105)
+    assert_equal Merchant, merchant.class
+    assert_equal "Shopin1901", merchant.name
+  end
+
+  def test_it_will_find_invoice_items_by_invoice
+    invoice_items = inv.find_invoice_items_by_invoice_id(1)
+    assert_equal Array, invoice_items.class
+    assert_equal InvoiceItem, invoice_items[0].class
+    assert_equal 1, invoice_items[0].id
+  end
+
+  def test_it_will_find_customer_by_invoice_id
+    customer = inv.find_customer_by_invoice_customer_id(1)
+    assert_equal Customer, customer.class
+    assert_equal 1, customer.id
+  end
+
+  def test_it_will_find_transactions_by_invoice
+    transaction = inv.find_transactions_by_invoice_id(1)
+    assert_equal Array, transaction.class
+    assert_equal Transaction, transaction[0].class
+    assert_equal 2650, transaction[0].id
+  end
+
+  def test_it_will_find_items_by_invoice
+    items = inv.find_items_by_invoice_id(263395237)
+    assert_equal Item, items.class
+    assert_equal 263395237,items.id
   end
 
 end
